e2c589e18a2ef8edf2a9b988fbb418112dd030df
Vic-Hndz-PC <victor.hernandez06@goumh.umh.es>
2025-03-20 18:23:53 +0100

    Refactor schema validation: Update pressureLevels and areaCovered fields for improved validation and structure

--- shared/schema/schema.ts
+++ shared/schema/schema.ts
@@ -2,7 +2,14 @@ import { pgTable, serial, text, boolean, integer, timestamp, jsonb } from "drizz
 import { createInsertSchema } from "drizzle-zod";
 import { z } from "zod";
 import { VariableEnum, TypesEnum, RangesEnum, FormatEnum } from "@shared/enums/requests.enums";
-import { stringToNumberArray, stringToStringArray } from "@shared/utils/stringConvertion";
+import {
+  advancedPressureLevelsOptions,
+  simplePressureLevelsOptions,
+} from "@shared/consts/pressureLevelsOptions";
+
+function isSubsetOf(arr: number[], set: Set<number>): boolean {
+  return arr.every((item) => set.has(item));
+}
 
 export const users = pgTable("users", {
   id: serial("id").primaryKey(),
@@ -47,53 +54,36 @@ export const insertRequestSchema = createInsertSchema(requests).extend({
   variableName: z.enum([VariableEnum.Geopotential, VariableEnum.Temperature], {
     required_error: "Variable name is required",
   }),
-  pressureLevels: z.preprocess(
-    stringToNumberArray,
-    z.number().array().min(1, "At least one pressure level is required")
-  ),
-  // .refine(
-  //   (arr) =>
-  //     isSubsetOf(arr, simplePressureLevelsOptions) ||
-  //     isSubsetOf(arr, advancedPressureLevelsOptions),
-  //   {
-  //     message: "pressureLevels must only contain valid values (simple or advanced sets).",
-  //   }
-  // ),
-  years: z.preprocess(
-    stringToNumberArray,
-    z.number().array().min(1, "At least one year is required")
-  ),
-  months: z.preprocess(
-    stringToNumberArray,
-    z.number().array().min(1, "At least one month is required")
-  ),
-  days: z.preprocess(
-    stringToNumberArray,
-    z.number().array().min(1, "At least one day is required")
-  ),
-  hours: z.preprocess(
-    stringToNumberArray,
-    z.number().array().min(1, "At least one hour is required")
-  ),
-  areaCovered: z.preprocess(
-    stringToNumberArray,
-    z.number().array().length(4, "Area must have exactly 4 values").default([90, -180, -90, 180])
-  ),
-  mapTypes: z.preprocess(
-    stringToStringArray,
-    z
-      .enum([TypesEnum.Cont, TypesEnum.Disp, TypesEnum.Comb, TypesEnum.Forms])
-      .array()
-      .min(1, "At least one type is required")
-  ),
-  mapRanges: z.preprocess(
-    stringToStringArray,
-    z
-      .enum([RangesEnum.Max, RangesEnum.Min, RangesEnum.Both, RangesEnum.Comb])
-      .array()
-      .min(1, "At least one range is required")
-  ),
-  mapLevels: z.preprocess(stringToNumberArray, z.number().array().optional().default([20])),
+  pressureLevels: z
+    .number()
+    .array()
+    .min(1, "At least one pressure level is required")
+    .refine(
+      (arr) =>
+        isSubsetOf(arr, simplePressureLevelsOptions) ||
+        isSubsetOf(arr, advancedPressureLevelsOptions),
+      {
+        message: "pressureLevels must only contain valid values (simple or advanced sets).",
+      }
+    ),
+  years: z.number().array().min(1, "At least one year is required"),
+  months: z.number().array().min(1, "At least one month is required"),
+  days: z.number().array().min(1, "At least one day is required"),
+  hours: z.number().array().min(1, "At least one hour is required"),
+  areaCovered: z
+    .number()
+    .array()
+    .length(4, "Area must have exactly 4 values")
+    .default([90, -180, -90, 180]),
+  mapTypes: z
+    .enum([TypesEnum.Cont, TypesEnum.Disp, TypesEnum.Comb, TypesEnum.Forms])
+    .array()
+    .min(1, "At least one type is required"),
+  mapRanges: z
+    .enum([RangesEnum.Max, RangesEnum.Min, RangesEnum.Both, RangesEnum.Comb])
+    .array()
+    .min(1, "At least one range is required"),
+  mapLevels: z.number().array().optional().default([20]),
   fileFormat: z
     .enum([FormatEnum.PNG, FormatEnum.JPG, FormatEnum.JPEG, FormatEnum.PDF, FormatEnum.SVG])
     .optional()
